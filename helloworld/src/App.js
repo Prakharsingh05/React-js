bee717f074664e53f69b61aaca59c12d75144232
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-14 22:21:18 +0530

    updated

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,15 +1,19 @@
-
+import React,{Component} from 'react'
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 
-function App() {
+class App extends Component {
+  render(){
   return (
     <div className="App">
       <Greet/>
       <Welcome></Welcome>
+      <Hello></Hello>
     </div>
   );
+}
 }
 
 export default App;
---
+++ helloworld/src/components/hello.js
@@ -0,0 +1,12 @@
+import react from 'react'
+
+const Hello= ()=> {
+
+    return (
+        <div>
+            <h1>Hello prakhar again</h1>
+        </div>
+    )
+}
+
+export default Hello;
