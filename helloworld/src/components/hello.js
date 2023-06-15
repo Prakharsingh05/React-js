0f57c355e23077d89307874a9f2b36405cdd9b85
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-15 14:18:49 +0530

    updated

--- helloworld/src/components/hello.js
+++ helloworld/src/components/hello.js
@@ -1,12 +1,16 @@
-import react from 'react'
+import React from 'react'
 
 const Hello= ()=> {
 
     return (
-        <div>
+       <div>
             <h1>Hello prakhar again</h1>
         </div>
     )
+
+    // lets write this again woithout jsx
+
+    // return React.createElement('div', null ,'hello Prakhar without using jsx')
 }
 
 export default Hello;
