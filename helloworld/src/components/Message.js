9d4250282a3dec82d1d80abc5d8dad55d9342a50
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-15 15:09:00 +0530

    updated

---
+++ helloworld/src/components/Message.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+ class  Message extends Component {
+  render() {
+    return (
+    
+    <h1>
+        Welcome Visitor
+    </h1>
+
+
+    )
+
+    
+  }
+}
+
+export default Message;
