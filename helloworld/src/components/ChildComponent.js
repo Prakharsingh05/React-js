8e0ef88f14de04e49d4fdc74e0a5cb59118d0712
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-19 20:50:42 +0530

    update

---
+++ helloworld/src/components/ChildComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ChildComponent extends Component {
+  render() {
+    return (
+      <div>
+        <button>GreetParent</button>
+      </div>
+    )
+  }
+}
+
+export default ChildComponent
