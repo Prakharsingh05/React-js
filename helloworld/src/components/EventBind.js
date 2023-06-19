dbdff394c4ea9534a2b75dd60c55bbe04bb5f3cd
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-19 20:40:15 +0530

    updated

--- helloworld/src/components/EventBind.js
+++ helloworld/src/components/EventBind.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
 
 export class EventBind extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         message: 'Hello'
+      }
+    }
+      clickHandler(){
+        this.setState({
+            message: 'goodbye'
+      })
+      }
+    
+    
   render() {
     return (
       <div>
+        {this.state.message}
+        <button onClick ={this.clickHandler.bind(this)}>click-EB</button>
         
       </div>
     )
