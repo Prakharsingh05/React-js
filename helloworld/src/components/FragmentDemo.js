3fad441ebf623849d795805a805c023ebe19ffd9
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-20 23:01:23 +0530

    updated

--- helloworld/src/components/FragmentDemo.js
+++ helloworld/src/components/FragmentDemo.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 function FragmentDemo() {
   return (
-    <div>
+    <React.Fragment>
+    <h1>
       Fragment Demo
-    </div>
+    </h1>
+    <p>This describes the fragment demo tagp</p>
+    </React.Fragment> 
   )
 }
 
