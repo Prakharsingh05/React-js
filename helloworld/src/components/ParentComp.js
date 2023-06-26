9e4502c4044bbba26d95a6b4d21e2a960a3a3657
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-26 21:16:35 +0530

    wdwd

--- helloworld/src/components/ParentComp.js
+++ helloworld/src/components/ParentComp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import RegComp from './RegComp'
 import PureComp from './PureComp'
+import MemoComp from './MemoComp'
 
 class ParentComp extends Component {
     constructor(props) {
@@ -23,8 +24,10 @@ class ParentComp extends Component {
     return (
       <div>
         Parent compoenent
-        <RegComp name ={this.state.name}/>
-        <PureComp name ={this.state.name}/>
+        <MemoComp name = {this.state.name}/>
+        {/* <RegComp name ={this.state.name}/>
+        <PureComp name ={this.state.name}/> */}
+        
       </div>
     )
   }
