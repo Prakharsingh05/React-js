bf2979e12718d3aee2d6dbcb0a51cb398343bc5a
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-26 21:16:40 +0530

    wdwd

---
+++ helloworld/src/components/MemoComp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MemoComp(name) {
+    console.log('rendering memo');
+  return (
+    <div>
+      {name}
+    </div>
+  )
+}
+
+export default React.memo(MemoComp);
