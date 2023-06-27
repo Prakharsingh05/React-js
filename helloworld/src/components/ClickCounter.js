73867a735f33ea78928d3e16c51c0466743e1889
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-27 23:14:34 +0530

    Higherorderfunction

---
+++ helloworld/src/components/ClickCounter.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+class ClickCounter extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         count : 0
+      }
+    }
+
+    ClickHandler = ()=>{
+      
+      this.setState({
+        count: this.state.count+1
+      })
+    }
+    
+  render() {
+    return (
+      <div>
+        <button onClick={this.ClickHandler.bind(this)}>clicked {this.state.count} times</button>
+      </div>
+    )
+  }
+}
+
+export default ClickCounter
