81d3ac2bbcbf8d44143ea353942880e6e570abf1
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-28 00:09:40 +0530

    updated

--- helloworld/src/components/ClickCounter.js
+++ helloworld/src/components/ClickCounter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import UpdatedComponent from './withCounter'
 
 class ClickCounter extends Component {
     constructor(props) {
@@ -25,4 +26,4 @@ class ClickCounter extends Component {
   }
 }
 
-export default ClickCounter
+export default UpdatedComponent(ClickCounter)
