b1ed82dd0b276417822e4b935b7b78a71e777db7
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-28 13:28:55 +0530

    done

--- learninghooks/src/Components/HookCounter3.js
+++ learninghooks/src/Components/HookCounter3.js
@@ -6,14 +6,15 @@ function HookCounter3() {
     <form>
       <input type='text' 
        value ={name.firstName}
-       onChange={e => setName({firstName : e.target.value})}
+       onChange={e => setName({...name ,firstName : e.target.value})}
        d/>
       <input type='text'
        value ={name.lastName}
-       onChange={e => setName({lastName : e.target.value})}
+       onChange={e => setName({...name , lastName : e.target.value})}
        />
       <h2>your first name is -{name.firstName}</h2>
-      <h2>your Last name is -{name.lastdName}</h2>
+      <h2>your Last name is -{name.lastName}</h2>
+      <h2>{JSON.stringify(name)}</h2>
     </form>
   )
 }
