c2de7c619ba0f295805782f577771d460dc6ae7b
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-28 20:40:10 +0530

    createdArray

---
+++ Projects/chatminds/src/AiOptions/index.jsx
@@ -0,0 +1,66 @@
+const arrayItems = [{
+    name : 'Q&A',
+    id : "q&a",
+    description : "Answer question based on existing Knowledge",
+},
+{
+    name : 'Grammer correction',
+    id : "grammercorrection",
+    description : "Corrects Sentences into standard English",
+},
+{
+    name : 'Text to command',
+    id : "texttocommand",
+    description : "Translate text into programmatic commands",
+},
+{
+    name : 'Movie to emoji',
+    id : "movietoemoji",
+    description : "Convert movie titles into emoji",
+},
+{
+    name : 'Calculate Time Complexity',
+    id : "timecomplexity",
+    description : "Find the time complexity of a function",
+},
+{
+    name : 'Translate programming languages',
+    id : "tpl",
+    description : "Translate from one programming language to another",
+},
+{
+    name : 'Explain code',
+    id : "explaincode",
+    description : "Answer question based on existing Knowledge",
+},
+{
+    name : 'Extract Keywords',
+    id : "extractkeywords",
+    description : "Extract Keywords from a block of text",
+},
+{
+    name : 'Mood to color',
+    id : "moodtocolor",
+    description : "Turn a description into a color",
+},
+
+{
+    name : 'Javascript One line Function',
+    id : "JavascripttoOneline",
+    description : "Turn a javaScript function into a one liner",
+},
+
+{
+    name : 'Create horror story',
+    id : "Createhorror ",
+    description : "rreates two to three sentence short horror stories",
+},
+
+{
+    name : 'Chat',
+    id : "Chat",
+    description : "Open ended coversation with an AI assistant",
+}
+
+
+]
