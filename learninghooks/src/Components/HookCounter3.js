feaa50dd5b41547d175cf63f1fc8a1c901a1e472
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-28 13:22:45 +0530

    dw

--- learninghooks/src/Components/HookCounter3.js
+++ learninghooks/src/Components/HookCounter3.js
@@ -1,15 +1,17 @@
 import React,{useState} from 'react'
 
 function HookCounter3() {
-    const[name,setName] = useState({firstName: "" ,lastName:""})
+    const[name,setName] = useState({firstName: "" , lastName:""})
    return (
     <form>
-      <input type='text' value ={name.firstName}
+      <input type='text' 
+       value ={name.firstName}
        onChange={e => setName({firstName : e.target.value})}
        d/>
-      <input type='text' value ={name.lastName}
+      <input type='text'
+       value ={name.lastName}
        onChange={e => setName({lastName : e.target.value})}
-       d/>
+       />
       <h2>your first name is -{name.firstName}</h2>
       <h2>your Last name is -{name.lastdName}</h2>
     </form>
