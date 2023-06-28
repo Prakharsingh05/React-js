f30d4d5e1a418df55f83adb5987b1ba50991f266
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-28 21:38:21 +0530

    options

--- Projects/chatminds/src/App.jsx
+++ Projects/chatminds/src/App.jsx
@@ -1,18 +1,23 @@
-
+import React, {useState} from 'react';
 import './App.css';
 import OptionSelection from './Components/OptionSelection';
 import {arrayItems} from "./AiOptions";
+import Translation from './Components/Translation';
 
 function App() {
+  const [option,setOption] = useState({});
   const selectOption =(Option)=>{
     console.log(Option);
   };
+
+  console.log(option)
+
 
 
   return (
     <div className="App">
       <OptionSelection arrayItems={arrayItems} selectOption={selectOption}/>
-      
+      <Translation/>
     </div>
   );
 }
--- Projects/chatminds/src/Components/OptionSelection.jsx
+++ Projects/chatminds/src/Components/OptionSelection.jsx
@@ -9,7 +9,7 @@ export default function OptionSelection({arrayItems , selectOption}) {
         <div className="grid-main">
           {arrayItems.map((item)=>{
 
-            return<div className='grid-child' onClick={selectOption}>
+            return<div className='grid-child' onClick={()=>selectOption(item.option)}>
             <h3>{item.name}</h3>
             <p>{item.description}</p>
             </div>
