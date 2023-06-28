d1deefeaf0eb2c0258103977c726bf9fa91c49d1
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-28 11:44:54 +0530

    useStatePrevState

--- learninghooks/src/App.js
+++ learninghooks/src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 import ClassCounter from './Components/ClassCounter';
 import HookCounter from './Components/HookCounter';
+import HookCounter2 from './Components/HookCounter2';
 
 function App() {
   return (
     <div className="App">
+      <HookCounter2/>
       <HookCounter/>
      <ClassCounter/>
     </div>
--- learninghooks/src/Components/HookCounter2.js
+++ learninghooks/src/Components/HookCounter2.js
@@ -2,11 +2,20 @@ import React,{useState} from 'react'
 
 function HookCounter2() {
     const initialCount = 0
-    const [count , setCount] =useState
+    const [count , setCount] =useState(initialCount)
+    
+    const incrementFive=()=>{
+        for(let i = 0; i<5; i++){
+        setCount(prevCount => prevCount + 1)
+        }
+    }
   return (
     <div>
         count : {count}
-        <button>Increment</button>
+        <button onClick={()=> setCount(initialCount)}>Resetd</button>
+        <button onClick={()=> setCount(count+1)}>Increment</button>
+        <button onClick={()=> setCount(count-1)}>Decrement</button>
+        <button onClick ={incrementFive}>Increment 5</button>
          
     </div>
   )
