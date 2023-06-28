3f1ccf5cbbcd41f846c21de6db722b94a09d0cb3
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-28 13:20:08 +0530

    useStatewithObject

--- learninghooks/src/App.js
+++ learninghooks/src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import ClassCounter from './Components/ClassCounter';
 import HookCounter from './Components/HookCounter';
 import HookCounter2 from './Components/HookCounter2';
+import HookCounter3 from './Components/HookCounter3';
 
 function App() {
   return (
     <div className="App">
+      <HookCounter3/>
       <HookCounter2/>
       <HookCounter/>
      <ClassCounter/>
