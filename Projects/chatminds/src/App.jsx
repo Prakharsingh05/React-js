17b0cac31181cb38b83e872420d574711b8df462
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-28 20:42:44 +0530

    a

--- Projects/chatminds/src/App.jsx
+++ Projects/chatminds/src/App.jsx
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import OptionSelection from './Components/OptionSelection';
+import {arrayItems} from "./AiOptions";
 
 function App() {
+  console.log(arrayItems);
 
   return (
     <div className="App">
