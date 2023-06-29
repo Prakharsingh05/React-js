8ab6f72b3c69422349bf34c8c2c2ce275376e216
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-29 19:48:40 +0530

    save

--- Projects/techmech/src/App.js
+++ Projects/techmech/src/App.js
@@ -1,26 +1,27 @@
-import {BrowserRouter as Router , Route, Routes} from "react-router-dom";
-import Header from "./componenets/Header";
-import Home from "./componenets/Home"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
 
-
-
-
-import "./styles/App.scss"
-import "./styles/header.scss"
-import "./styles/home.scss"
-
-
+import "./styles/App.scss";
+import "./styles/header.scss";
+import "./styles/home.scss";
+import "./styles/footer.scss";
+import "./styles/contact.scss";
+import "./styles/mediaquery.scss";
 
 function App() {
   return (
     <Router>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path ="/" element={<Home/>}/>
-
-
-        
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
