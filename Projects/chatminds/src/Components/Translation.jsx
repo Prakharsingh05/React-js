8efcb2d98e5fc19e4d14c5db45d63aea2012b630
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-29 11:30:31 +0530

    a

--- Projects/chatminds/src/Components/Translation.jsx
+++ Projects/chatminds/src/Components/Translation.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
-export default function Translation(sendToAI) {
+export default function Translation(sendToAI ,setInput ,result) {
   return (
     <div className='container col-6'>
-      <textarea className="text-area" cols={55} rows={10} onChange={(e)=>setInput(e.target.value)}>
+      <textarea className="text-area" cols={55} rows={10} onChange={(e)=> setInput(e.target.value)}>
 
       </textarea>
     <br/>
       <button onClick ={sendToAI} type="button" className="btn btn-success col-6 ">Send to AI</button>
       <button type="button" className="btn btn-danger col-4">Reset</button>
+      <h3 className="result-text">{result.length >0 ? result: ""}</h3>
     </div>
   )
 }
