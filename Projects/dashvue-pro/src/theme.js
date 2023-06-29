db5617df7ac7f32a25c73162d19a9edfdcbdacfa
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-29 22:08:54 +0530

    Started new project

---
+++ Projects/dashvue-pro/src/theme.js
@@ -0,0 +1,10 @@
+import {createContext , useState , useMemo} from "react"
+import {createTheme} from "@mui/material/sstyles";
+
+// color design tokens
+
+#666666
+#141b2d
+#4cceac
+#db4f4a
+#6870fa
