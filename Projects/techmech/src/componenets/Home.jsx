986d946a12294b4861b9711fa561dd2937ab075d
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-29 18:23:45 +0530

    save

--- Projects/techmech/src/componenets/Home.jsx
+++ Projects/techmech/src/componenets/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import vg from "../assets/2.jpg"
 
 const Home = () => {
   return (
+    <>
     <div className='home'>
       Home
 
@@ -10,6 +12,19 @@ const Home = () => {
         <p>Solution to all your problems</p>
       </main>
     </div>
+
+
+
+    <div className="home2">
+        <img src={vg} alt="Graphics"/>
+        <div>
+            <p>
+            At techmech, we are dedicated to being your premier solution provider, offering a comprehensive range of services to meet your diverse needs. We understand that every business faces unique challenges, and we are here to provide tailored solutions that drive your success.
+            With years of industry expertise
+            </p>
+        </div>
+    </div>
+    </>
   )
 }
 
