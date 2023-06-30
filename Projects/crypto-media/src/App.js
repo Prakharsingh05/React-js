da8ab35a04ba610cea82f9e8e53ba89e86388e9b
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-30 12:01:04 +0530

    navbaradded

--- Projects/crypto-media/src/App.js
+++ Projects/crypto-media/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import {Switch ,Route , Link} from "react-router-dom";
+import {Layout , Typography , space} from 'antd';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div>
-        <h1>Crypto Media</h1>
-      
+    <div className="app">
+        <div className="navbar">
+            <Navbar/>
+
+        </div>
+        <div className="main">
+
+        </div>
+        <div className="footer">
+
+        </div>
     </div>
   )
 }
---
+++ Projects/crypto-media/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Button , Menu , Typography , Avatar } from 'antd';
+import{Link} from 'react-router-dom';
+import { HomeOutlined , MoneyCollectOutlined , BulbOutlined, MenuOutlined} from '@ant-design/icons';
+import icon from "../images/cryptocurrency.png"
+
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+        <div className="logo-container">
+            <Avatar src={icon} size ="large"/>
+            <Typography.Title level={2} className='logo'>
+                <Link to="/">Crypto Media</Link>
+   
+            </Typography.Title>
+            
+            <button className="menu-control-container">
+
+            </button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
