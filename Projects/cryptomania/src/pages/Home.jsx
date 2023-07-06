059801dbd03ad02f679e1f410267cc16c55a5c47
Prakhar Singh <prakharsingh285@gmail.com>
2023-07-06 05:44:20 +0530

    updated

--- Projects/cryptomania/src/pages/Home.jsx
+++ Projects/cryptomania/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>
+    <main className='w-full h-full flex flex-col first-letter: content-center items-center relative text-white font-nunito'>
       home
-    </div>
+      <div className='w-screen h-screen bg-gray-300 fixed -z-10'>
+
+      </div>
+      <Outlet/>
+    </main>
   )
 }
 
