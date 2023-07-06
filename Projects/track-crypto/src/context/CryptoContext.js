0fff502e09956e5d22f437a5f46ad59763103f41
Prakhar Singh <prakharsingh285@gmail.com>
2023-07-06 21:09:57 +0530

    API added

--- Projects/track-crypto/src/context/CryptoContext.js
+++ Projects/track-crypto/src/context/CryptoContext.js
@@ -6,9 +6,21 @@ export const CryptoContext = createContext({});
 
 // create the provider component
 
+
 export const CryptoProvider=({children})=>{
+     const {cryptoData , setCryptoData} = useState();
                 
-         
+        const getCryptoData = async () =>{
+
+            try{
+                const data =await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en`)
+
+
+            }catch(error){
+                console.log("Error getting crypto data", error);
+            }
+
+        } 
 
     return(
 
