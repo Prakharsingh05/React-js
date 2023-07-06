67c2ea358d490d2daf6b47651715e771043e55aa
Prakhar Singh <prakharsingh285@gmail.com>
2023-07-06 16:17:12 +0530

    save

--- Projects/track-crypto/src/index.js
+++ Projects/track-crypto/src/index.js
@@ -3,11 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    children:{
+      path:"/",
+      element: <Crypto/>
+    }
+  }
+]);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
