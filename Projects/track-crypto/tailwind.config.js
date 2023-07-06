eb385975084acb0aca2daed9c53cc81c93c70e50
Prakhar Singh <prakharsingh285@gmail.com>
2023-07-06 15:56:27 +0530

    a

--- Projects/track-crypto/tailwind.config.js
+++ Projects/track-crypto/tailwind.config.js
@@ -10,11 +10,11 @@ module.exports = {
 
     colors: {
 
-gray: { 100: "#808080", 200: "#323232", 300: "#212121" },
-white: "#fff",
-cyan: "#14ffec",
-red: "#d6436e",
-green: "#25da72",     
+  gray: { 100: "#808080", 200: "#323232", 300: "#212121" },
+  white: "#fff",
+  cyan: "#14ffec",
+  red: "#d6436e",
+  green: "#25da72",     
     },
 
 
