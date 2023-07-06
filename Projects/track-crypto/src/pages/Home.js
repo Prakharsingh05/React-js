7fc4c3153a12422fd429a612f84861220fdc3427
Prakhar Singh <prakharsingh285@gmail.com>
2023-07-06 17:36:21 +0530

    nav

--- Projects/track-crypto/src/pages/Home.js
+++ Projects/track-crypto/src/pages/Home.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Logo from '../components/Logo'
+import Navigation from '../components/Navigation'
 
 const Home = () => {
   return (
-    <div>
-      HOME
+    <main className="w-full h-200 flex flex-col first-letter:first-line:
+    content-center items-center relative text-white font-nunito
+    ">
+      <div className ="w-screen h-screen bg-gray-300 fixed -z-10"/>
+      <Logo/>
+      <Navigation/>
       <Outlet/>
-    </div>
+    </main>
   )
 }
 
