cb3ba5f41a45a0c7559018ddd1e7e335ca6f53bb
Prakhar Singh <prakharsingh285@gmail.com>
2023-07-17 21:07:15 +0530

    json added

--- Projects/track-crypto/src/context/CryptoContext.js
+++ Projects/track-crypto/src/context/CryptoContext.js
@@ -14,7 +14,7 @@ export const CryptoProvider=({children})=>{
         const getcryptoData = async()=>{
             try{
                 const data = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en
-                `)
+                `).then(res=>res.json()).then(json=>json);
                 
             }catch(error){
                 console.log(error);
@@ -26,7 +26,7 @@ export const CryptoProvider=({children})=>{
 
     return(
 
-        <CryptoContext.Provider value={{test}}>
+        <CryptoContext.Provider value={{cryptoData}}>
 
         {children}
         </CryptoContext.Provider>
