55501c44830c9fe30a7f7166028e507573642fb5
Prakhar Singh <prakharsingh285@gmail.com>
2023-07-17 21:13:33 +0530

    updated

--- Projects/track-crypto/src/context/CryptoContext.js
+++ Projects/track-crypto/src/context/CryptoContext.js
@@ -1,4 +1,4 @@
-import {createContext,  useState} from "react";
+import {createContext,  useLayoutEffect,  useState} from "react";
 
 // create context object
 
@@ -9,17 +9,22 @@ export const CryptoContext = createContext({});
 
 export const CryptoProvider=({children})=>{
 
-     const [cryptoData , setCryptoData] = useState();   
+    const [cryptoData , setCryptoData] = useState();   
 
         const getcryptoData = async()=>{
             try{
-                const data = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en
-                `).then(res=>res.json()).then(json=>json);
+                const data = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en`).then(res=>res.json()).then(json=>json);
+                console.log(data);
+                setCryptoData(data);
                 
             }catch(error){
                 console.log(error);
             }
-        }
+        };
+
+        useLayoutEffect(()=>{
+            getcryptoData();
+        },[])
 
                 
         
