495406abe0ab1c2571c45d67ad34703ea1f19b81
Prakhar Singh <prakharsingh285@gmail.com>
2023-07-19 23:24:32 +0530

    search bar use context added

--- Projects/track-crypto/src/components/Search.js
+++ Projects/track-crypto/src/components/Search.js
@@ -2,6 +2,14 @@ import React from 'react'
 import searchIcon from "../assets/search-icon.svg";
 
 function Search() {
+
+  const [seacrhText, setSearchText] = useState("");
+
+  let handleInput = (e) =>{
+    e.preventDefayult();
+    let query = e.target.value;
+    console.log(query);
+  }
   return (
     <form className='w-96  relative flex items-center
     
@@ -9,6 +17,7 @@ function Search() {
 
     '>
         <input type='text' nama="search"
+        onChange={handleInput}
         className='w-full rounded bg-gray-200
            placeholder: text-gray-100 pl-2
            required outline-0 border border-transparent 
