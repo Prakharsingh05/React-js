93a228ced88067bcecd352bd33f01cc505858f69
Prakhar Singh <prakharsingh285@gmail.com>
2023-07-24 21:58:30 +0530

    makin coin search

--- Projects/track-crypto/src/context/CryptoContext.js
+++ Projects/track-crypto/src/context/CryptoContext.js
@@ -11,6 +11,20 @@ export const CryptoProvider=({children})=>{
 
     const [cryptoData , setCryptoData] = useState();   
     const [searchData , setSearchData] = useState();   
+    const [coinSearch , setCoinSearch] = useState("");
+
+
+    const getCoinData = async()=>{
+        try{
+            const data = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24%2C7d&locale=en`).then(res=>res.json()).then(json=>json);
+            console.log(data);
+            setCryptoData(data);
+            
+        }catch(error){
+            console.log(error);
+        }
+    };
+
 
         const getcryptoData = async()=>{
             try{
